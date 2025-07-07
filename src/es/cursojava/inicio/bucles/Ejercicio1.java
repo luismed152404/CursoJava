17a383d2292d990bc58a11c0a226edf3749cd610
Tardes <Tardes@GET-12>
2025-07-07 19:58:15 +0200

    Aumento del valor de la variable i de 100 a 10000

--- src/es/cursojava/inicio/bucles/Ejercicio1.java
+++ src/es/cursojava/inicio/bucles/Ejercicio1.java
@@ -4,7 +4,7 @@ public class Ejercicio1 {
 
 	public static void main(String[] args) {
 //		Mostrar los numeros pares del 0 al 100
-		for (int i=0 ; i<100 ;i++) {
+		for (int i=0 ; i<10000 ;i++) {
 			
 		if (i%2==0) {
 			
@@ -13,7 +13,7 @@ public class Ejercicio1 {
 			}
 	}
 		
-		for (int i=0, j=99 ; i<100 ; i+=2, j-=2)
+		for (int i=0, j=99 ; i<10000 ; i+=2, j-=2)
 		{System.out.println(i+"-"+j);		}
 	}
 }
